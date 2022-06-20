5f776febb88d4167a82aee0f92570cc573bb10d2
KibaFrosty <kirillnovakovskii98@gmail.com>
2022-06-20 21:14:18 +0300

    add js

---
+++ front/js/course/02_variables.js
@@ -0,0 +1,58 @@
+var firstName = "Test"
+const lastName = "TestTest"
+let age = 25
+
+firstName = "Max"
+console.log(firstName);
+
+age = 27
+console.log (age);
+
+const COLOR = '#f5f5f5';
+COLOR = '000000';
+console.log (COLOR); //error tk constanta ne perezapisivaetsa
+
+
+if (true) {
+    var a = 45 //45
+}
+
+if (true) {
+    let a = 45
+}
+console.log(a); //error
+
+let a = 24;
+if (true) {
+    let a = 45
+    console.log(a);
+}
+console.log (a);
+
+console.log(Math, P1);
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(2, 3));
+console.log(Math.pow(2, 53-1)); 
+
+console.log(Math.floor(4.9)); //okr v mensh
+console.log(Math.ceil(4.9)); //okr v bolsh
+
+if (count=15) {
+  console.log('Cool')  
+} else {
+  console.log ('Bad')    
+}
+
+if (count=15) {
+    console.log('Cool') 
+} else if (count == 25) {
+    console.log('Uwu')
+}
+else {
+    console.log('Bad')
+}
+
+count == 15 ?  console.log ('Cool') : console.log('Bad');
+
+    
